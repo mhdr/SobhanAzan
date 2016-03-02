3d1e594db32782cfd5711051d01210547311fe99
Mahmood <ramzani.mahmood@gmail.com>
2016-03-02 22:16:28 +0330

    find time of azan zohr for today

--- Azan/src/PlayAzan/Main.java
+++ Azan/src/PlayAzan/Main.java
@@ -4,23 +4,57 @@ import javafx.application.Application;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+import org.joda.time.DateTime;
 
 import java.io.File;
-import java.nio.file.Paths;
+import java.sql.*;
 
 /**
  * Created by Mahmood on 3/2/2016.
  */
 public class Main extends Application{
-    public static void main(String[] args){
-        Application.launch(args);
-        //Media media=new Media(new File("Azan1.mp3").toURI().toString());
-        //MediaPlayer mediaPlayer=new MediaPlayer(media);
-        //mediaPlayer.play();
+    public static void main(String[] args) throws SQLException {
+        //Application.launch(args);
+
+        Azan azan = getAzan();
+
+        System.out.println(azan.toString());
+    }
+
+    private static Azan getAzan() throws SQLException {
+        DateTime today=new DateTime();
+        String todayStr=today.toString("yyyy-MM-dd");
+
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:Azan.db");
+
+        Statement statement = connection.createStatement();
+
+        ResultSet rs= statement.executeQuery("SELECT * FROM Times;");
+        Azan azan=null;
+
+        while (rs.next())
+        {
+            int azanId=rs.getInt("AzanId");
+            String azanDateTime=rs.getString("AzanDateTime");
+            int azanType=rs.getInt("AzanType");
+
+            if (azanDateTime.startsWith(todayStr) && azanType==2)
+            {
+                azan=new Azan(azanId,azanDateTime,azanType);
+            }
+
+        }
+
+        rs.close();
+        statement.close();
+        connection.close();
+        return azan;
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+
         Media media=new Media(new File("Azan1.mp3").toURI().toString());
         MediaPlayer mediaPlayer=new MediaPlayer(media);
         mediaPlayer.play();
