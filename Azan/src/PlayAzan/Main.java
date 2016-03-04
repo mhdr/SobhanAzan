492500e69aa6ed925378578b90d87a61d45899aa
Mahmood <ramzani.mahmood@gmail.com>
2016-03-04 18:16:03 +0330

    working on Scheduler

--- Azan/src/PlayAzan/Main.java
+++ Azan/src/PlayAzan/Main.java
@@ -5,6 +5,8 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 import org.joda.time.DateTime;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.File;
 import java.sql.*;
@@ -13,15 +15,27 @@ import java.sql.*;
  * Created by Mahmood on 3/2/2016.
  */
 public class Main extends Application{
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, SchedulerException {
         //Application.launch(args);
 
-        Azan azan = getAzan();
+        JobDetail job= JobBuilder.newJob(MainJob.class).withIdentity("Main","Main").build();
+
+        SimpleTrigger trigger= TriggerBuilder.newTrigger()
+                .withIdentity("Main","Main")
+                .startNow()
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInHours(1).repeatForever())
+                .build();
+
+        Scheduler scheduler=new StdSchedulerFactory().getScheduler();
+        scheduler.start();
+        scheduler.scheduleJob(job,trigger);
+
+        Azan azan = getNextAzan();
 
         System.out.println(azan.toString());
     }
 
-    private static Azan getAzan() throws SQLException {
+    private static Azan getNextAzan() throws SQLException {
         DateTime today=new DateTime();
         String todayStr=today.toString("yyyy-MM-dd");
 
@@ -53,8 +67,6 @@ public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
-
         Media media=new Media(new File("Azan1.mp3").toURI().toString());
         MediaPlayer mediaPlayer=new MediaPlayer(media);
         mediaPlayer.play();
---
+++ Azan/src/PlayAzan/MainJob.java
@@ -0,0 +1,16 @@
+package PlayAzan;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+/**
+ * Created by Mahmood on 3/4/2016.
+ */
+public class MainJob implements Job{
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+
+    }
+}
