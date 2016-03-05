efd703e7e2dc23e50e8a23c1ab0a77c912262ab2
Mahmood <ramzani.mahmood@gmail.com>
2016-03-06 00:08:23 +0330

    PlayJob and TurnOnSpeakerJob

---
+++ Azan/src/com/nasimeshomal/TurnOnSpeakerJob.java
@@ -0,0 +1,15 @@
+package com.nasimeshomal;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+/**
+ * Created by Mahmood on 3/5/2016.
+ */
+public class TurnOnSpeakerJob implements Job {
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+
+    }
+}
