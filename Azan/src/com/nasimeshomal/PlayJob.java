bc4c82c9566eb16773fb17f36114fa01bb65ab2a
Mahmood <ramzani.mahmood@gmail.com>
2016-03-19 14:32:42 +0330

    add more exception handling add windows Installer

--- Azan/src/com/nasimeshomal/PlayJob.java
+++ Azan/src/com/nasimeshomal/PlayJob.java
@@ -5,9 +5,12 @@ import javazoom.jl.player.Player;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Date;
 
 /**
  * Created by Mahmood on 3/5/2016.
@@ -15,12 +18,20 @@ import java.io.FileNotFoundException;
 public class PlayJob implements Job {
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
+        Logger logger= LoggerFactory.getLogger(TurnOnSpeakerJob.class);
+
         try {
+            logger.info("%s : Azan is playing now ...",new Date().toString());
+
             this.PlayAzan();
+            logger.info("%s : End of playing Azan.",new Date().toString());
+
             Thread.sleep(4000);
             RaspGPIO.getInstance().getSpeakerPin().low();
+            logger.info("%s : Speaker is Off.",new Date().toString());
         } catch (InterruptedException e) {
             e.printStackTrace();
+            logger.trace("%s : Exeption while playing Azan ==> %s",new Date().toString(),e.getMessage());
         }
     }
 
